b9c68b554dea593eb09b55444796e4f6e3ef6b6e
ybyuan <3118918283@qq.com>
2024-12-11 22:09:15 +0800

    release: version 0.1.0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mltt-resolver",
-    version="0.1.0.0.1",
+    version="0.1.0.0.2",
     packages=find_packages(),
     install_requires=[],
     extras_require={
